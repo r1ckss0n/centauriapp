14d22cb1b11f6c422e0b4bdd332a41b76ad30056
Ricksson Ferreira <nosskcir@outlook.com>
2021-05-20 17:26:18 -0300

    modified app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os, psycopg2
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.congig['SECRET_KEY']= 'secret'
-app.config['SQLACHEMY_DATABASE_URI']=os.environ.get("DATABASE_URL")
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# set up database
+app.congif['SECRET_KEY']= 'secret'
+app.config['SQLACHEMY_DATABASE_URI']=os.getenv("DATABASE_URL")
 
 db=SQLAlchemy(app)
 
